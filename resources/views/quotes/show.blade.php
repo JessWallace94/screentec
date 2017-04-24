376c9bb7dea6c36f0d00aceee03d563a06e0fd3c
vagrant <vagrant@homestead>
2017-04-24 17:01:49 +0000

    fixed links

--- resources/views/quotes/show.blade.php
+++ resources/views/quotes/show.blade.php
@@ -6,12 +6,12 @@
       <div class="col-md-3">
         <ul class="nav nav-pills nav-stacked">
             <li>MAIN</li>
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="#">Customers</a></li>
-            <li><a href="#">Vehicles</a></li>
-            <li><a href="#">Quotes</a></li>
+            <li><a href="{{ url('/') }}">Dashboard</a></li>
+            <li><a href="{{ url('/customers') }}">Customers</a></li>
+            <li><a href="{{ url('/vehicles') }}">Vehicles</a></li>
+            <li><a href="{{ url('/quotes') }}">Quotes</a></li>
             <li>MORE</li>
-            <li><a href="#">Support</a></li>
+            <li><a href="{{ url('/support') }}">Support</a></li>
         </ul>
       </div>
       <div class="col-md-9">
